9d4bf8cf8061904b19b44732224263dada1303ff
LinYan <linyan@primeton.com>
2017-10-12 16:37:31 +0800

    add empty

---
+++ L171012_pm/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  L171012_pm
+//
+//  Created by LinYan on 12/10/2017.
+//  Copyright © 2017 LinYan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ L171012_pm/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  L171012_pm
+//
+//  Created by LinYan on 12/10/2017.
+//  Copyright © 2017 LinYan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
